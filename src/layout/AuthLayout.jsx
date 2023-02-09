7496c51a85f4f05d26a33e302e0b5fedb3ffe894
Luis Daniel <ls.ortizdaniel@gmail.com>
2023-02-09 13:20:03 -0600

    after creating the login form

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 const AuthLayout = () => {
   return (
     <>
-        <main className="container mx-auto md:grid md:grid-cols-2 mt-8">
+        <main className="container mx-auto md:grid md:grid-cols-2 mt-10 gap-14 p-4">
           <Outlet/>
         </main>
         
--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -9,8 +9,42 @@ const Login = () => {
           </h1>
         </div>
         <div>
-          2
+          <form action="">
+            <div className="my-4">
+              <label 
+                className="uppercase text-gray-500 block text-xl font-bold" 
+                htmlFor="">
+                  Email
+              </label>
+              <input 
+                type="email"
+                placeholder="Email de Registro" 
+                className="border w-full p-3 mt-3 bg-gray-200 rounded-2xl"
+              />
+            </div>
+            <div className="my-4">
+              <label 
+                className="uppercase text-gray-500 block text-xl font-bold" 
+                htmlFor="">
+                  Password
+              </label>
+              <input 
+                type="password"
+                placeholder="Contraseña" 
+                className="border w-full p-3 mt-3 bg-gray-200 rounded-2xl"
+              />
+            </div>
+
+            <input 
+              type = "submit"
+              value = "Inicar Sesión"
+              className = "bg-amber-600 w-full py-3 px-10 rounded-2xl text-slate-100 uppercase font-bold mt-5 hover: cursor-pointer hover:bg-amber-400 hover:text-indigo-700 md:w-auto "  
+            />
+
+
+          </form>
         </div>
+
     </>
   )
 }
