c686c592083bb5986abfff20664b7e5253f769ba
Luis Daniel <ls.ortizdaniel@gmail.com>
2023-02-15 21:17:02 -0600

    after first steps with reset password-send email

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Login from './paginas/Login'
 import Registrar from './paginas/Registrar' 
 import OlvidePassword from './paginas/OlvidePassword' 
 import ConfirmarCuenta from './paginas/ConfirmarCuenta' 
+import NuevoPassword from './paginas/NuevoPassword'
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
               <Route index element = {<Login/>}/> {/* index es la pagina principal */}
               <Route path="registrar" element = {<Registrar/>}/>
               <Route path="olvide-password" element = {<OlvidePassword/>}/>
+              <Route path="olvide-password/:token" element = {<NuevoPassword/>}/>
               <Route path="confirmar/:id" element = {<ConfirmarCuenta/>}/>
         </Route>
       </Routes>
---
+++ src/paginas/NuevoPassword.jsx
@@ -0,0 +1,8 @@
+
+const NuevoPassword = () => {
+  return (
+    <div>desde nuevo password</div>
+  )
+}
+
+export default NuevoPassword
