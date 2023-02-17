12c1f64626757627f2c330f8fcbb79db7126d605
Luis Daniel <ls.ortizdaniel@gmail.com>
2023-02-17 07:55:30 -0600

    aftr create an authentication context

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,23 @@ import OlvidePassword from './paginas/OlvidePassword'
 import ConfirmarCuenta from './paginas/ConfirmarCuenta' 
 import NuevoPassword from './paginas/NuevoPassword'
 
+import { AuthProvider } from './context/AuthProvider'
+
 function App() {
 
   return (
     <BrowserRouter  >
-      <Routes  >
-        <Route  path="/" element = {<AuthLayout/>}>
-              <Route index element = {<Login/>}/> {/* index es la pagina principal */}
-              <Route path="registrar" element = {<Registrar/>}/>
-              <Route path="olvide-password" element = {<OlvidePassword/>}/>
-              <Route path="olvide-password/:token" element = {<NuevoPassword/>}/>
-              <Route path="confirmar/:id" element = {<ConfirmarCuenta/>}/>
-        </Route>
-      </Routes>
+      <AuthProvider>
+        <Routes  >
+          <Route  path="/" element = {<AuthLayout/>}>
+                <Route index element = {<Login/>}/> {/* index es la pagina principal */}
+                <Route path="registrar" element = {<Registrar/>}/>
+                <Route path="olvide-password" element = {<OlvidePassword/>}/>
+                <Route path="olvide-password/:token" element = {<NuevoPassword/>}/>
+                <Route path="confirmar/:id" element = {<ConfirmarCuenta/>}/>
+          </Route>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   )
 }
